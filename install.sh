1b17fec7ff0d8f3a47ea3fad4bbe6dd510f5752d
zhenyuqiaotencent <zhenyuqiao@tencent.com>
2020-11-23 16:26:24 +0800

    Create install.sh
    添加安装脚本

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# from https://github.com/razeencheng/git-hooks
+
+set -e
+
+_green() {
+  printf '\033[1;31;32m%b\033[0m\n' "$1"
+}
+
+_red() {
+  printf '\033[1;31;40m%b\033[0m\n' "$1"
+}
+
+_exists() {
+  cmd="$1"
+  if [ -z "$cmd" ]; then
+    _red "Usage: _exists cmd"
+    return 1
+  fi
+
+  if eval type type >/dev/null 2>&1; then
+    eval type "$cmd" >/dev/null 2>&1
+  elif command >/dev/null 2>&1; then
+    command -v "$cmd" >/dev/null 2>&1
+  else
+    which "$cmd" >/dev/null 2>&1
+  fi
+  return "$?"
+}
+
+main() {
+  if [ "$(uname -s)" != "Darwin" ]; then
+    _red "Unsupported operating system, Darwin?";
+    return 1;
+  fi
+
+  if [ ! -d ~/.git_template/hooks ]; then
+    mkdir -p ~/.git_template/hooks
+  fi 
+
+
+  if [ -f ~/.git_template/hooks/commit-msg ]; then
+    mv ~/.git_template/hooks/commit-msg ~/.git_template/hooks/commit-msg.bak;
+  fi
+  
+
+  curl https://git.code.oa.com/zhenyuqiao/git-hooks/blob/master/git_commit_msg.sh -o ~/.git_template/hooks/commit-msg
+
+  git config --global init.templatedir ~/.git_template
+
+  _green "Golang pre-commit hook & commit-msg hook Install Success!"
+}
+
+main "$@"
