4a466109fd3e7f4d12f5c4c9ee5c77171f02dd00
zhenyuqiaotencent <zhenyuqiao@tencent.com>
2021-11-29 20:10:13 +0800

    Update git_commit_msg.sh

--- git_commit_msg.sh
+++ git_commit_msg.sh
@@ -11,7 +11,7 @@ _red() {
   printf '\033[1;31;31m%b\033[0m\n' "$1"
 }
 
-COMMIT_MSG=`cat $1 | egrep "^(feat|fix|docs|chore|style|refactor|test|other)?:(\S|\w)+"`
+COMMIT_MSG=`cat $1 | egrep "^(feat|fix|docs|chore|style|refactor|test|other)?:\s(\S|\w)+"`
 
 if [ "$COMMIT_MSG" = "" ]; then
     _red "message 不是以feat，fix，docs，chore，style，refactor，test，other开头，请修改备注\n"
